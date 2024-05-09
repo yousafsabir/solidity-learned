bb15e20a282c40f338e5bde585c4cc7ec690adf3
yousafsabir <myousafdev@gmail.com>
2024-05-10 03:00:39 +0500

    feat: add owner only change tweet length function

--- Twitter.sol
+++ Twitter.sol
@@ -15,9 +15,27 @@ struct Tweet {
 }
 
 contract Twitter {
+    uint16 public MAX_TWEET_LENGTH = 280;
+    address owner;
     mapping(address => Tweet[]) public tweets;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner, "You're not the Owner");
+        _;
+    }
+
+    function changeTweetLimit(uint16 newLimit) external ownerOnly {
+        MAX_TWEET_LENGTH = newLimit;
+    }
+
     function createTweet(string memory _tweet) external {
+        // restricting tweet length to 280 characters only
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long");
+
         tweets[msg.sender].push(Tweet(msg.sender, _tweet, block.timestamp, 0));
     }
 
@@ -28,11 +46,11 @@ contract Twitter {
         return tweets[_person][_number];
     }
 
-    function likeTweet(address _author, uint _tweet) external {
-        tweets[_author][_tweet].likes++;
-    }
-
     function getTweets(address _person) external view returns (Tweet[] memory) {
         return tweets[_person];
+    }
+
+    function likeTweet(address _author, uint _tweet) external {
+        tweets[_author][_tweet].likes++;
     }
 }
